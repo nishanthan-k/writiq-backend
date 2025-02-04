3e269de8a8da5a6329340b0c54fc5f0b4a1799ea
nishanthan-k <krn.nishanthan@gmail.com>
2025-02-04 06:23:08 +0530

    feat: Repo creation

--- src/main/java/com/writiq/blog/domain/entities/TagRepo.java
+++
@@ -1,9 +0,0 @@
-package com.writiq.blog.domain.entities;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TagRepo extends MongoRepository<Tag, String> {
-
-}
---
+++ src/main/java/com/writiq/blog/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.writiq.blog.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.writiq.blog.domain.entities.Category;
+
+@Repository
+public interface CategoryRepository extends MongoRepository<Category, String> {
+
+}
